23e4cac1e393e22ac5159f96a5c7b9cab424d02a
Suman Navaratnarajah <v115be17@alex1.nhr.fau.de>
2025-06-13 09:49:11 +0200

    feat(ascii): added option to add row identifier

--- src_code/model_utils/deepseek.py
+++ src_code/model_utils/deepseek.py
@@ -13,14 +13,15 @@ NUM_EVAL = 100
 
 # model_name="deepseek-ai/DeepSeek-R1-Distill-Qwen-14B" # not possible with a100
 # model_name = "deepseek-ai/DeepSeek-R1-0528"
-model_name  = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
-# model_name = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+# model_name  = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+model_name = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
 # model_name = "meta-llama/Llama-3.1-8B-Instruct"
 grid_sizes = [3, 8, 9, 10]
 pipe = pipeline("text-generation", model=model_name, max_length=8192)
+add_start_end_row_identifier=True
 for grid_size in grid_sizes:
     out_example = True # whether to include the output example or not
-    pure_language = True # whether language or ascii
+    pure_language = False # whether language or ascii
 
     pure_language_str = "language" if pure_language else "ascii"
     data = {"model_name": model_name,
@@ -36,8 +37,10 @@ for grid_size in grid_sizes:
     
     st_time = time.time()
     responses = ""
+    
+    file_name = f"results_{model_name.split('/')[1]}_{pure_language_str}_{grid_size}x{grid_size}_out_example_{out_example}_add_start_end_row_identifier{add_start_end_row_identifier}.txt"
     for i in range(NUM_EVAL):
-        with open(f"results_{model_name.split('/')[1]}_{pure_language_str}_{grid_size}x{grid_size}_out_example_{out_example}.txt", "a+") as fo:
+        with open(file_name, "a+") as fo:
             st_time_temp = time.time()
             img_rgb1, grid_world1 = dataset[i]
             prompt = prompt_generator(grid_world1, pure_language=pure_language, img=None, img_symbol="<image>", out_example=out_example)
@@ -54,10 +57,11 @@ for grid_size in grid_sizes:
             gt = str(grid_world1.a_star())
             responses += extracted_response + "\n"
             fo.write(f"### {str(i)} ###" + "\n" + result + "\n" + "time_taken: " + f"{time.time() - st_time_temp}" + "\n")
+            fo.write(f"### prompt ###" + "\n" + prompt + "\n")
     et_time = time.time()
 
     # final details 
-    with open(f"results_{model_name.split('/')[1]}_{pure_language_str}_{grid_size}x{grid_size}_out_example_{out_example}.txt", "a+") as fo:
+    with open(file_name, "a+") as fo:
         fo.write(f"avg_time_for_inference: {(et_time - st_time)/NUM_EVAL}")
         fo.write(f"\n{str(data)}")
         fo.write(f"Extracted Responses: \n {responses}")
